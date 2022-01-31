e0ad6fc726b645f7c44fc7f5516375a295c6353c
MikeA <Mikeashley227@yahoo.com>
2022-01-31 15:37:00 -0500

    Refactored to singleton structure + support for logging

--- src/main/java/com/ourfallenfriend/materiallist/messenger/Messenger.java
+++ src/main/java/com/ourfallenfriend/materiallist/messenger/Messenger.java
@@ -1,17 +1,48 @@
 package com.ourfallenfriend.materiallist.messenger;
 
+import com.ourfallenfriend.materiallist.Main;
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.TextColor;
 import org.bukkit.entity.Player;
-import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 
 public class Messenger {
-    public static void dispatch(@NotNull Player receiver, @NotNull MessageType messageType, String message) {
-        Component prefix = messageType.getPrefixComplete();
-        TextColor messageColor = messageType.getMessageColor();
-        Component convertedMessage = Component.text(message).color(messageColor);
-        Component completedMessage = prefix.append(Component.text(" ")).append(convertedMessage);
+    public static final Messenger instance;
+    private final Logger logger;
 
+    private Messenger() {
+        logger = Main.getInstance().getLogger();
+        Main.getInstance().getLogger().log(Level.INFO, "Messenger Instantiated.");
+    }
+
+    static {
+        instance = new Messenger();
+    }
+
+    public static Messenger getInstance() {
+        return instance;
+    }
+
+    public void sendMessage(Player receiver, MessageType messageType, String message) {
+        boolean wasSuccessful = dispatch(receiver, messageType, message);
+        if(!wasSuccessful) {
+            logger.log(Level.WARNING, "Attempted to dispatch message to null player!");
+        }
+    }
+
+    private boolean dispatch(Player receiver, MessageType messageType, String message) {
+        if(Objects.isNull(receiver)) return false;
+        Component completedMessage = buildCompleteMessage(messageType, message);
         receiver.sendMessage(completedMessage);
+        return true;
+    }
+
+    private Component buildCompleteMessage(MessageType mT, String msg) {
+        Component prefix = mT.getPrefixComplete();
+        Component convertedMessage = Component.text(msg).color(mT.getMessageColor());
+        return prefix.append(Component.text(" ")).append(convertedMessage);
     }
 }
