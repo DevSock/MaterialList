44e7d0a365a3d5ffb0fb402028cac001c4704055
MikeA <Mikeashley227@yahoo.com>
2022-02-01 19:00:40 -0500

    Implemented component message param.

--- src/main/java/com/ourfallenfriend/materiallist/messenger/Messenger.java
+++ src/main/java/com/ourfallenfriend/materiallist/messenger/Messenger.java
@@ -2,6 +2,7 @@ package com.ourfallenfriend.materiallist.messenger;
 
 import com.ourfallenfriend.materiallist.Main;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.entity.Player;
 
 import java.util.Objects;
@@ -27,7 +28,7 @@ public class Messenger {
         return instance;
     }
 
-    public void sendMessage(Player receiver, MessageType messageType, String message, Boolean shouldLog){
+    public void sendMessage(Player receiver, MessageType messageType, Boolean shouldLog, Component message){
         boolean wasSuccessful = dispatch(receiver, messageType, message);
         if(shouldLog) {
             logger.log(Level.INFO, buildCompleteMessage(messageType, message).toString());
@@ -37,12 +38,20 @@ public class Messenger {
         }
     }
 
+    public void sendMessage(Player receiver, MessageType messageType, Component message) {
+        sendMessage(receiver, messageType, false, message);
+    }
+
+    public void sendMessage(Player receiver, MessageType messageType, Boolean shouldLog, String message) {
+        sendMessage(receiver, messageType, shouldLog, Component.text(message));
+    }
+
     public void sendMessage(Player receiver, MessageType messageType, String message) {
-        sendMessage(receiver, messageType, message, false);
+        sendMessage(receiver, messageType, false, message);
     }
 
     public void sendMessage(Player receiver, Boolean shouldLog, BakedMessage bakedMessage) {
-        sendMessage(receiver, bakedMessage.getMessageType(), bakedMessage.getMessage(), shouldLog);
+        sendMessage(receiver, bakedMessage.getMessageType(), shouldLog, bakedMessage.getMessage());
     }
 
     public void sendMessage(Player receiver, BakedMessage bakedMessage) {
@@ -58,16 +67,17 @@ public class Messenger {
         sendMessage(receiver, false, bakedMessages);
     }
 
-    private boolean dispatch(Player receiver, MessageType messageType, String message) {
+    private boolean dispatch(Player receiver, MessageType messageType, Component message) {
         if(Objects.isNull(receiver)) return false;
         Component completedMessage = buildCompleteMessage(messageType, message);
         receiver.sendMessage(completedMessage);
         return true;
     }
 
-    private Component buildCompleteMessage(MessageType mT, String msg) {
+    private Component buildCompleteMessage(MessageType mT, Component msg) {
         Component prefix = mT.getPrefixComplete();
-        Component convertedMessage = Component.text(msg).color(mT.getMessageColor());
-        return prefix.append(Component.text(" ")).append(convertedMessage);
+        Component styledMessage = msg.color(mT.getMessageColor()).decoration(TextDecoration.BOLD, false);
+
+        return prefix.append(Component.text(" ")).append(styledMessage);
     }
 }
