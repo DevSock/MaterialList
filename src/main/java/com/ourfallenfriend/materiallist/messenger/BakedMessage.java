8ba9c45a2d0fd9fde6601dd4a6fc61e40b85d66e
MikeA <Mikeashley227@yahoo.com>
2022-02-01 19:10:45 -0500

    Shortened some messages for chat conformity

--- src/main/java/com/ourfallenfriend/materiallist/messenger/BakedMessage.java
+++ src/main/java/com/ourfallenfriend/materiallist/messenger/BakedMessage.java
@@ -5,12 +5,12 @@ import net.kyori.adventure.text.Component;
 public enum BakedMessage {
     WRONG_ARGS("Invalid command arguments, please try again.", MessageType.FAILURE),
     SERVER_CONNECTION_FAILED("Server connection failed, please inform the system administrators.", MessageType.WARN),
-    NOT_CONTRACTED("You aren't currently under a material list contract.", MessageType.FAILURE),
-    ALREADY_CONTRACTED("You're already under a material list contract.", MessageType.FAILURE),
-    CONTRACT_STARTED("You're now under a material list contract, and your block interactions are being tracked!", MessageType.SUCCESS),
-    CONTRACT_HOWTO_END("In order to end your contract, type /MaterialList Stop.", MessageType.TIP),
-    CONTRACT_SUBMITTED("Your material list contract has been submitted!", MessageType.INFO),
-    CONTRACT_ENDED("Your material list contract has been completed!", MessageType.SUCCESS),
+    NOT_CONTRACTED("You aren't currently making a material list.", MessageType.FAILURE),
+    ALREADY_CONTRACTED("You're already under making a material list.", MessageType.FAILURE),
+    CONTRACT_STARTED("You're now making a material list, your block interactions are being monitored!", MessageType.SUCCESS),
+    CONTRACT_HOWTO_END("To stop making a material list type /MaterialList Stop.", MessageType.TIP),
+    CONTRACT_SUBMITTED("Your material list has been submitted!", MessageType.INFO),
+    CONTRACT_ENDED("Your material list has been completed!", MessageType.SUCCESS),
     COMMAND_USAGE("/MaterialList <Start / Stop>", MessageType.INFO);
 
     private final String message;
